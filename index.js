ad81aa48d12a6135fa931a33333c6bf1eacf2d04
Isaac Kyuma <isaackyuma56@gmail.com>
2025-09-09 09:07:18 +0300

    Complete arrow functions expressions lab

---
+++ index.js
@@ -0,0 +1,8 @@
+// 1. Function called divide that divides 2000 by 100
+const divide = () => 2000 / 100;
+
+// 2. Function called square that squares a number
+const square = x => x * x;
+
+// 3. Function called add that adds two numbers
+const add = (a, b) => a + b;
